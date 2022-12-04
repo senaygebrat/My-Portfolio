d15f33acf7c38c9042c0bf3c6455cb654e7b8ec2
Senay Gebrat <senaygebrat@gmail.com>
2022-12-04 12:41:42 -0800

    styling change to about page

--- my-portfolio/src/components/pages/About.js
+++ my-portfolio/src/components/pages/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className="about-page">
       <h2 className='intro'>A Little About Me....</h2>
-      <div className='box'>
+      <div className='custom-box'>
         <img className='selfie' src={senay} alt='self-portrait'></img>
       <p className='summary'>I'm a native Californian that has lived in Atlanta since 2009. Make sure to fill in plenty more of text telling the world who I am and what I love. My favorite sport is basketball and I watch the NBA champion Golden State Warriors faithfully! I'm a native Californian that has lived in Atlanta since 2009. Make sure to fill in plenty more of text telling the world who I am and what I love. My favorite sport is basketball and I watch the NBA champion Golden State Warriors faithfully!</p>
       </div>
