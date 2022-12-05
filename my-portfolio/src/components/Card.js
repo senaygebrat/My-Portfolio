7348b0c48f31ddcb103e6602ceed5d06fa85703d
Senay Gebrat <senaygebrat@gmail.com>
2022-12-04 21:07:07 -0800

    added all projects

--- my-portfolio/src/components/Card.js
+++ my-portfolio/src/components/Card.js
@@ -12,7 +12,7 @@ export default function Card(props) {
       {/* <div className='card-header'>Senay</div> */}
       <div className='card-body'>
         <h3 className='text-center'>{props.name}</h3>
-        <img width='250px' src={props.image}></img>
+        <img width='250vw' src={props.image}></img>
         <br></br>
         <div className='d-flex flex-row justify-content-around'>
         <a href={props.github}>Github</a>
--- my-portfolio/src/components/pages/Portfolio.js
+++ my-portfolio/src/components/pages/Portfolio.js
@@ -3,17 +3,20 @@ import Card from '../Card';
 import workday from '../../assets/images/Work-Day-Scheduler.gif';
 import mickey from '../../assets/images/mickey-n-friends.gif';
 import passwordGen from '../../assets/images/Password-Generator.gif';
+import weather from '../../assets/images/Weather-Dashboard.gif';
+import codingQ from '../../assets/images/Coding-Quiz.gif';
+import horiseon from "../../assets/images/horiseon.png";
 
 function Portfolio() {
   return (
     <div className='container'>
       <div className='row d-flex justify-content-between'>
       <Card name={"Scheduler"} image = {workday} github = {"https://github.com/senaygebrat/Work-Day-Scheduler"} deployed = {"https://senaygebrat.github.io/Work-Day-Scheduler/"} />
-      <Card name={"Mickey N Friends"} image = {mickey} github = {"https://github.com/wseban/Mickey-n-Friends-Finder"} deployed = {"https://wseban.github.io/Mickey-n-Friends-Finder/"} />
-      <Card name={"Project 3"} image = {passwordGen} github = {"https://github.com/senaygebrat/Password-Generator"} deployed = {"https://senaygebrat.github.io/Password-Generator/"} />
-      <Card name={"Project 4"} image = {workday} github = {"https://github.com/senaygebrat/Work-Day-Scheduler"} deployed = {"https://senaygebrat.github.io/Work-Day-Scheduler/"} />
-      <Card name={"Project 5"} image = {workday} github = {"https://github.com/senaygebrat/Work-Day-Scheduler"} deployed = {"https://senaygebrat.github.io/Work-Day-Scheduler/"} />
-      <Card name={"Project 6"} image = {workday} github = {"https://github.com/senaygebrat/Work-Day-Scheduler"} deployed = {"https://senaygebrat.github.io/Work-Day-Scheduler/"} />
+      <Card name={"Mickey Finder"} image = {mickey} github = {"https://github.com/wseban/Mickey-n-Friends-Finder"} deployed = {"https://wseban.github.io/Mickey-n-Friends-Finder/"} />
+      <Card name={"Generator"} image = {passwordGen} github = {"https://github.com/senaygebrat/Password-Generator"} deployed = {"https://senaygebrat.github.io/Password-Generator/"} />
+      <Card name={"Weather"} image = {weather} github = {"https://github.com/senaygebrat/Weather-Dashboard"} deployed = {"https://senaygebrat.github.io/Weather-Dashboard/"} />
+      <Card name={"Coding Quiz"} image = {codingQ} github = {"https://github.com/senaygebrat/Code-Quiz"} deployed = {"https://senaygebrat.github.io/Code-Quiz/"} />
+      <Card name={"Horiseon"} image = {horiseon} github = {"https://github.com/senaygebrat/Horiseon-access"} deployed = {"https://senaygebrat.github.io/Horiseon-access//"} />
       </div>
     </div>
   )
