195b3449e9219ddf761a8a83cebb2f0f9c2703e6
Senay Gebrat <senaygebrat@gmail.com>
2022-12-10 11:03:00 -0800

    added to about

--- my-portfolio/src/components/pages/About.js
+++ my-portfolio/src/components/pages/About.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import Typed from "react-typed";
 import senay from '../../assets/images/senay.png';
 
 function About() {
   return (
+ 
     <div className="about-page content-color">
-      <h2 className='intro'>A Little About Me....</h2>
+      <h2 className='intro'>
+      <Typed
+      strings={[
+            "A Little About Me",
+          ]}
+          typeSpeed={50}
+          backSpeed={100}
+          loop
+        />
+      </h2>
       <div className='custom-box'>
         <img className='selfie' src={senay} alt='self-portrait'></img>
       <p className='summary'>I'm a native Californian that has lived in Atlanta since 2009. Some of my favorite hobbies include coding, gaming, and watching sports. I've loved computers since a young age and always knew that I would want to passionately pursue a career in this field. My goal is to build my own custom PC in the next year and take my gaming experience to the next level. My favorite sport is basketball and I watch the NBA champion Golden State Warriors faithfully! </p>
       </div>
     </div>
+    
   )
 }
+
 
 export default About
